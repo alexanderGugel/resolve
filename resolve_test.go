1b150f0bfeae63421f0724a99a7456b4c581f18f
Alexander Gugel <alexander.gugel@gmail.com>
2015-06-19 22:24:02 +0200

    test: Use testCase struct

--- resolve_test.go
+++ resolve_test.go
@@ -5,62 +5,66 @@ import (
 	"testing"
 )
 
-func TestResolveExisting(t *testing.T) {
-	pwd, err := os.Getwd()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	testcases := map[string]string{
-		"./test/hello.js":                      "test/hello.js",
-		"./test/hello":                         "test/hello.js",
-		"./test/other_file.js":                 "test/other_file.js",
-		"./test/other_file":                    "test/other_file.js",
-		"./test/just_dir/hello_1":              "test/just_dir/hello_1.js",
-		"./test/just_dir/hello_2":              "test/just_dir/hello_2.js",
-		"./test/just_dir/index":                "test/just_dir/index.js",
-		"./test/just_dir":                      "test/just_dir/index.js",
-		"./test/module_with_main":              "test/module_with_main/main.js",
-		"./test/module_with_main/package.json": "test/module_with_main/package.json",
-		"./test/module_without_main":           "test/module_without_main/index.js",
-	}
-
-	for required, resolved := range testcases {
-		dependency, err := Resolve(required, pwd)
-
-		if err != nil {
-			t.Errorf("got error %q when resolving %q, expected nil", err, required)
-		}
-
-		expected := pwd + string(os.PathSeparator) + resolved
-
-		if dependency == nil {
-			t.Errorf("got no dependency when resolving %q; expected %q", required, expected)
-			continue
-		}
-
-		if dependency.Pathname != expected {
-			t.Errorf("got dependency %q when resolving %q; expected %q", dependency.Pathname, required, expected)
-		}
-	}
+type testCase struct {
+	require  string
+	from     string
+	resolved string
 }
 
-func TestResolveMissing(t *testing.T) {
+func TestResolveExisting(t *testing.T) {
 	pwd, err := os.Getwd()
+
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	testcases := []string{"./test/not_here/", "./test/somewhere_else", "./test/not_found/module.js"}
+	testCases := []testCase{
+		testCase{"./test/hello.js", pwd, "test/hello.js"},
+		testCase{"./test/hello", pwd, "test/hello.js"},
+		testCase{"./test/other_file.js", pwd, "test/other_file.js"},
+		testCase{"./test/other_file", pwd, "test/other_file.js"},
+		testCase{"./test/just_dir/hello_1", pwd, "test/just_dir/hello_1.js"},
+		testCase{"./test/just_dir/hello_2", pwd, "test/just_dir/hello_2.js"},
+		testCase{"./test/just_dir/index", pwd, "test/just_dir/index.js"},
+		testCase{"./test/just_dir", pwd, "test/just_dir/index.js"},
+		testCase{"./test/module_with_main", pwd, "test/module_with_main/main.js"},
+		testCase{"./test/module_with_main/package.json", pwd, "test/module_with_main/package.json"},
+		testCase{"./test/module_without_main", pwd, "test/module_without_main/index.js"},
+		testCase{"./test/not_here/", pwd, ""},
+		testCase{"./test/somewhere_else", pwd, ""},
+		testCase{"./test/not_found/module.js", pwd, ""},
+	}
 
-	for _, required := range testcases {
-		dependency, err := Resolve(required, pwd)
+	for _, testCase := range testCases {
+		dependency, err := Resolve(testCase.require, testCase.from)
 
-		if err == nil {
-			t.Errorf("got no error when resolving %q; expected error", required)
-		}
-		if dependency != nil {
-			t.Errorf("got dependency %q when resolving %q; expected nil", dependency.Pathname, required)
+		if testCase.resolved == "" {
+
+			if err == nil {
+				t.Errorf("got no error when resolving %q; expected error", testCase.require)
+			}
+
+			if dependency != nil {
+				t.Errorf("got dependency %q when resolving %q; expected nil", dependency.Pathname, testCase.require)
+			}
+
+		} else {
+
+			if err != nil {
+				t.Errorf("got error %q when resolving %q, expected nil", err, testCase.require)
+			}
+
+			expected := pwd + string(os.PathSeparator) + testCase.resolved
+
+			if dependency == nil {
+				t.Errorf("got no dependency when resolving %q; expected %q", testCase.require, expected)
+				continue
+			}
+
+			if dependency.Pathname != expected {
+				t.Errorf("got dependency %q when resolving %q; expected %q", dependency.Pathname, testCase.require, expected)
+			}
+
 		}
 	}
 }
